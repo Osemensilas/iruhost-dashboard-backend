9c5a85d6f761de32c72bd6557713f135dc030440
unknown <osemensilas@gmail.com>
2026-09-06 12:15:37 +0100

    New Commit

--- App/Controllers/API/DashboardController.php
+++ App/Controllers/API/DashboardController.php
@@ -7,10 +7,6 @@ use PDO;
 use PDOException;
 use Dotenv\Dotenv;
 
-require_once __DIR__ . '/../../../vendor/PHPMailer/src/Exception.php';
-require_once __DIR__ . '/../../../vendor/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/../../../vendor/PHPMailer/src/SMTP.php';
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
