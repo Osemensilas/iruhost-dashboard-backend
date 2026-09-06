8309691e24f5f687100cb5a3272c06b258137665
unknown <osemensilas@gmail.com>
2026-09-06 09:58:24 +0100

    New Commit

--- App/Controllers/API/SessionController.php
+++ App/Controllers/API/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\API;
 
 use App\Core\DB;
+use PDO;
 
 class SessionController{
     private $pdo;
@@ -38,7 +39,32 @@ class SessionController{
             return;
         }
         
-        echo $_SESSION['admin']['user_id'];
+        if (!$_SESSION['admin']['user_id']){
+            echo json_encode([
+                'status' => 'error', 
+                'message' => 'No active session'
+            ]);
+            return;
+        }
+
+        $stmt = $this->pdo->prepare("SELECT * FROM admin_users WHERE user_id = ?");
+        $stmt->execute([$_SESSION['admin']['user_id']]);
+
+        if ($stmt->rowCount() < 1){
+            echo json_encode([
+                'status' => 'error', 
+                'message' => 'User do not exist'
+            ]);
+            return;
+        }
+
+        $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode([
+            'status' => 'success', 
+            'message' => 'User retrieved successfully',
+            'user' => $rows
+        ]);
     }
 
     public function Logout(){
